e2eb62a38ea5872850585bdb215f98b3a0c28138
Avellin003 <133269087+Avellin003@users.noreply.github.com>
2024-01-19 12:15:19 +0200

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+#creates file school
+
 file { '/tmp/school':
   ensure  => file,
   mode    =>'0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
