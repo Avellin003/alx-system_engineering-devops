300784edef1199024bfc93bde3ef8af7ef0572f1
root <root@avellin.localdomain>
2024-02-19 21:20:05 +0200

    updates

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-"""Runs the REST API"""
-import urllib.request
-import json
+"""retuns the todo list of the employee"""
+import requests
 import sys
 
-
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com/todos"
-
-    """Fetching employee data"""
-    employee_url = f"{base_url}?userId={employee_id}"
-    with urllib.request.urlopen(employee_url) as response:
-        employee_data = json.load(response)
-
-    """calculating progress"""
-    total_tasks = len(employee_data)
-    completed_tasks = sum(task['completed'] for task in employee_data)
-
-    """Displaying progress"""
-    print(f"Employee {employee_data[0]['userId']} is done with tasks\
-            ({completed_tasks}/{total_tasks}):")
-    print(f"{employee_data[0]['userId']}: {employee_data[0]['title']}")
-
-    """Displaying completed tasks"""
-    for task in employee_data:
-        if task['completed']:
-            print(f"\t{task['title']}")
-
-
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
