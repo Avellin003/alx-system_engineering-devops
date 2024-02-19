4dd167c8f23f3b7f5a043ca5af6ea1cab0226711
root <root@avellin.localdomain>
2024-02-19 21:00:08 +0200

    updates

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     usr = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    complete = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [t.get("title") for t in todo if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         usr.get("name"), len(complete), len(todo)))
-    [print("\t {}".format(c)) for c in complete]
+    [print("\t {}".format(c)) for c in completed]
