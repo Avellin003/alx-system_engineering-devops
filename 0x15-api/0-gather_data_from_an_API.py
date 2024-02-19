50812aa698dd1b35b55992f4b2d49eed95e62e8b
root <root@avellin.localdomain>
2024-02-19 21:15:16 +0200

    updates

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,28 +4,32 @@ import urllib.request
 import json
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com/todos"
-    
+
     """Fetching employee data"""
     employee_url = f"{base_url}?userId={employee_id}"
     with urllib.request.urlopen(employee_url) as response:
         employee_data = json.load(response)
-    
+
     """calculating progress"""
     total_tasks = len(employee_data)
     completed_tasks = sum(task['completed'] for task in employee_data)
-    
+
     """Displaying progress"""
-    print(f"Employee {employee_data[0]['userId']} is done with tasks ({completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_data[0]['userId']} is done with tasks\
+            ({completed_tasks}/{total_tasks}):")
     print(f"{employee_data[0]['userId']}: {employee_data[0]['title']}")
-    
+
     """Displaying completed tasks"""
     for task in employee_data:
         if task['completed']:
             print(f"\t{task['title']}")
 
+
 if __name__ == "__main__":
+
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
