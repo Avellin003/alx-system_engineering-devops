566b166a689b28e6785fad31b3b84e15e1e8b27e
root <root@avellin.localdomain>
2024-03-06 21:48:48 +0200

    updates

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# the puppet script
+
+file {'copy_file':
+  ensure => file,
+  source => '/var/www/html/wp-includes/class-wp-locale.php',
+  path   => '/var/www/html/wp-includes/class-wp-locale.phpp',
+}
